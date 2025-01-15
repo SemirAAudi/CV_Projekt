faa303b2942efe036192931f2189f571e7b20ea6
Semir <semir-a@t-online.de>
2025-01-15 10:30:26 +0100

    Add header component, update pdf

--- AngularPortfolioWebsite/src/app/app.module.ts
+++ AngularPortfolioWebsite/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
