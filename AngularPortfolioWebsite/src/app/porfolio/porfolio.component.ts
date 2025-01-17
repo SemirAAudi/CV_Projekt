aebae49323e7037d92fb4339807e5ba4d19a5706
Semir <semir-a@t-online.de>
2025-01-17 15:32:17 +0100

    Add routing for navbar

--- AngularPortfolioWebsite/src/app/porfolio/porfolio.component.ts
+++ AngularPortfolioWebsite/src/app/porfolio/porfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-porfolio',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './porfolio.component.css'
 })
 export class PorfolioComponent {
-
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Semir Asceric - Portfolio')
+  }
 }
